b266667cca8b35da520280b01aff9a6d739565a7
linux <nino000semaan@hotmail.com>
2025-04-06 20:28:44 +0300

    created create-vehicle

---
+++ api/src/vehicle/dto/create-vehicle.dto.ts
@@ -0,0 +1,8 @@
+export class createVehicleDTO {
+    vehicleRegNum: number;
+    driverDriverID: number;
+    vehicleBrand: string;
+    vehicleModel: string;
+    vehicleType: number;
+    vehicleCapacity: number;
+}
