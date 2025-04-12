162402d67d93de862ec622231d6fefcb611751e5
linux <nino000semaan@hotmail.com>
2025-04-12 20:46:45 +0300

    created create member dto

---
+++ api/src/member/dto/create-member.dto.ts
@@ -0,0 +1,9 @@
+export class CreateMemberDto {
+    memberID: number;
+    memberTeamID: number;
+    memberTeamCampusID: number;
+    memberVehicleRegNum: number;
+    memberDriverID: number;
+    memberName: string;
+    memberNumber: number;
+}
